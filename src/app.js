088c2ac3214f925fe3e539350285ad2a58dde772
BrunoKings0 <bruno.vieiraxv@gmail.com>
2020-03-29 11:18:35 -0300

    inicial test deploy

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const routes = require('./routes')
-
+//use cors?
 app.use(bodyParser.json())
 
 app.use('', routes)
